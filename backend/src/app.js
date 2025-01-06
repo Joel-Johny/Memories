314ac4a061c9facf85e90c608942e2bebdcf641d
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2025-01-06 19:07:08 +0530

    middleware + test get/post data

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json()); // Parse incoming JSON data
+app.use(express.text()); // Parse incoming text data
+app.use(express.urlencoded({ extended: true })); // Parse incoming URL-encoded form-data
 
 // Connect to MongoDB
 mongoose
@@ -20,10 +22,14 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Basic Route
-app.get("/", (req, res) => {
+app.get("//test-get-data", (req, res) => {
   res.send("API is running...");
 });
 
+app.post("/test-post-data", (req, res) => {
+  console.log(req.body); // Will log: undefined
+  res.send("Data received");
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
