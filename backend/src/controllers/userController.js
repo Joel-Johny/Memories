60295e8b242a29844d5212f636ad164b54548345
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2025-01-07 12:09:19 +0530

    crud for users

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -29,4 +29,53 @@ const createUser = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers, createUser };
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+const updateUserById = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+};
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,18 @@
 // routes/userRoutes.js
 const express = require("express");
-const { getAllUsers, createUser } = require("../controllers/userController");
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} = require("../controllers/userController");
 const router = express.Router();
 
-router.get("/", getAllUsers);
-router.post("/", createUser);
+router.get("/getAll", getAllUsers);
+router.post("/create", createUser);
+router.get("/getUser/:id", getUserById);
+router.put("/updateUser/:id", updateUserById);
+router.delete("/deleteUser/:id", deleteUserById);
 
 module.exports = router;
