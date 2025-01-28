4fd587e6591902cf39e8046bb8c13b18a319f51d
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2025-01-28 16:19:41 +0530

    added route and controller to fetch journal by date

--- backend/src/routes/journalRoutes.js
+++ backend/src/routes/journalRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
   addOrUpdateJournal,
   getAllJournal,
+  journalByDate,
 } = require("../controllers/journalController");
 const { protect } = require("../middleware/authMiddleware");
 const { upload } = require("../middleware/multerCloudinaryMiddleware");
 
 // Protected route to add or update a journal entry
 router.post("/addOrUpdate", protect, upload, addOrUpdateJournal);
+router.get("/:date", protect, upload, journalByDate);
 router.get("/getJournals", protect, getAllJournal);
 
 module.exports = router;
