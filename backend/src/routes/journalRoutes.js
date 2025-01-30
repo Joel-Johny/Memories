1841a7336a59a740a3127a4987878578b905b1da
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2025-01-30 15:33:16 +0530

    **Refactor Journal Entry Handling:** * Created helper files for:     * `cleanupUploads`: Manages temporary file cleanup.     * `journalEntryValidation`: Centralizes journal entry validation.     * `cleanupCloudinaryData`: Handles Cloudinary resource deletion. * Refactored `addOrUpdateJournal` controller to utilize the new helper structure. * Implemented delete journal route and controller. * Modified `cleanupCloudinaryData` to accept the journal object instead of `userId` for more efficient cleanup.

--- backend/src/routes/journalRoutes.js
+++ backend/src/routes/journalRoutes.js
@@ -4,6 +4,7 @@ const {
   addOrUpdateJournal,
   getAllJournal,
   journalByDate,
+  deleteJournal,
 } = require("../controllers/journalController");
 const { protect } = require("../middleware/authMiddleware");
 const { upload } = require("../middleware/multerCloudinaryMiddleware");
@@ -12,5 +13,6 @@ const { upload } = require("../middleware/multerCloudinaryMiddleware");
 router.post("/addOrUpdate", protect, upload, addOrUpdateJournal);
 router.get("/:date", protect, upload, journalByDate);
 router.get("/getJournals", protect, getAllJournal);
+router.post("/deleteJournal", protect, deleteJournal);
 
 module.exports = router;
